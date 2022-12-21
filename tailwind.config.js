f7e1e6ba318f05a2c6417bcecd03d389e67b7614
innocces <18435105624@163.com>
2022-12-21 17:55:30 +0800

    chore(tailwindcss): support tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false, // disable Tailwind's reset
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/**/**/*.{js,jsx,ts,tsx}", "./docs/**/*.mdx", "./blog/*.mdx"],
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
